305a7fa757de99dbfc5d034112e51618c71c1b90
Alexey Obukhov <obukhov@cyberplat.com>
2018-04-25 15:07:06 +0300

    - Влиты изменения для php 7

--- iprivpg/src/php/phpipriv/phpipriv/ipriv.c
+++ iprivpg/src/php/phpipriv/phpipriv/ipriv.c
@@ -2,17 +2,28 @@
 // some instructions for Windows project: http://blog.slickedit.com/?p=128
 
 #ifdef _WIN32
-  #include "zend_config.w32.h"
+#include "zend_config.w32.h"
 #endif
 #include "php.h"
 #include "../../../libipriv.h"
 
-#define MAX_OVERHEAD	1024
+// fix for PHP 7
+#if PHP_MAJOR_VERSION < 7
+#define my_int int
+#define my_long long
+#define my_add_next_index_stringl(data, str, len, b) add_next_index_stringl(data, str, len, b)
+#else
+#define my_int size_t
+#define my_long zend_long
+#define my_add_next_index_stringl(data, str, len, b) add_next_index_stringl(data, str, len)
+#endif
+
+#define MAX_OVERHEAD	4096
 #define DEFAULT_ERROR	-1000
 
 /* declaration of functions to be exported */
-//ZEND_FUNCTION(DoubleUp);
 ZEND_FUNCTION(ipriv_sign);
+ZEND_FUNCTION(ipriv_signex);
 ZEND_FUNCTION(ipriv_verify);
 ZEND_FUNCTION(ipriv_sign2);
 ZEND_FUNCTION(ipriv_verify2);
@@ -25,6 +36,7 @@ ZEND_MSHUTDOWN_FUNCTION(ipriv);
 /* compiled function list so Zend knows what's in this module */
 zend_function_entry ipriv_functions[] = {
 	ZEND_FE(ipriv_sign, NULL)
+	ZEND_FE(ipriv_signex, NULL)
 	ZEND_FE(ipriv_verify, NULL)
 	ZEND_FE(ipriv_sign2, NULL)
 	ZEND_FE(ipriv_verify2, NULL)
@@ -44,23 +56,7 @@ zend_module_entry ipriv_module_entry = {
 	NO_VERSION_YET, STANDARD_MODULE_PROPERTIES
 };
 
-/* implement standard "stub" routine to introduce ourselves to Zend */
 ZEND_GET_MODULE(ipriv)
-
-/* DoubleUp function */
-/*
-ZEND_FUNCTION(DoubleUp)
-{
-	long theValue = 0;
-
-	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "l", &theValue) == FAILURE) {
-		RETURN_NULL();
-	}
-	theValue *= 2;
-
-	RETURN_LONG(theValue);
-}
-*/
 
 ZEND_MINIT_FUNCTION(ipriv)
 {
@@ -77,11 +73,11 @@ ZEND_MSHUTDOWN_FUNCTION(ipriv)
 ZEND_FUNCTION(ipriv_sign)
 {
 	char *content;
-	int content_len;
+	my_int content_len;
 	char *key;
-	int key_len;
+	my_int key_len;
 	char *pass;
-	int pass_len;
+	my_int pass_len;
 	IPRIV_KEY sec_key;
 	char *tmp = 0;
 	int rc = DEFAULT_ERROR;
@@ -107,7 +103,51 @@ ZEND_FUNCTION(ipriv_sign)
 
 	add_next_index_long(return_value, rc < 0 ? rc : 0);
 	if (rc > 0)
-		add_next_index_stringl(return_value, tmp, rc, 1);
+		my_add_next_index_stringl(return_value, tmp, rc, 1);
+	else
+		add_next_index_null(return_value);
+
+	if (tmp) free(tmp);
+}
+
+ZEND_FUNCTION(ipriv_signex)
+{
+	char *content;
+	my_int content_len;
+	char *key;
+	my_int key_len;
+	char *pass;
+	my_int pass_len;
+	my_int alg;
+	IPRIV_KEY sec_key;
+	char *tmp = 0;
+	int rc = DEFAULT_ERROR;
+
+	array_init(return_value);
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "sss|l", &content, &content_len,
+		&key, &key_len, &pass, &pass_len, &alg) == FAILURE) {
+		add_next_index_long(return_value, -1);
+		add_next_index_null(return_value);
+		return;
+	}
+	
+	if (alg != IPRIV_ALG_SHA256)
+		alg = IPRIV_ALG_MD5;
+	
+	rc = Crypt_OpenSecretKey(IPRIV_ENGINE_DEFAULT, key, key_len, pass, &sec_key);
+	if (!rc) {
+		tmp = (char *)malloc(content_len + MAX_OVERHEAD);
+		if (tmp) {			
+			rc = Crypt_SignEx(content, content_len, tmp, content_len + MAX_OVERHEAD, &sec_key, alg);
+			if (!rc)
+				rc = DEFAULT_ERROR - 1;
+		}
+		Crypt_CloseKey(&sec_key);
+	}
+
+	add_next_index_long(return_value, rc < 0 ? rc : 0);
+	if (rc > 0)
+		my_add_next_index_stringl(return_value, tmp, rc, 1);
 	else
 		add_next_index_null(return_value);
 
@@ -117,11 +157,11 @@ ZEND_FUNCTION(ipriv_sign)
 ZEND_FUNCTION(ipriv_sign2)
 {
 	char *content;
-	int content_len;
+	my_int content_len;
 	char *key;
-	int key_len;
+	my_int key_len;
 	char *pass;
-	int pass_len;
+	my_int pass_len;
 	IPRIV_KEY sec_key;
 	char *tmp = 0;
 	int rc = DEFAULT_ERROR;
@@ -147,7 +187,7 @@ ZEND_FUNCTION(ipriv_sign2)
 
 	add_next_index_long(return_value, rc < 0 ? rc : 0);
 	if (rc > 0)
-		add_next_index_stringl(return_value, tmp, rc, 1);
+		my_add_next_index_stringl(return_value, tmp, rc, 1);
 	else
 		add_next_index_null(return_value);
 
@@ -157,14 +197,14 @@ ZEND_FUNCTION(ipriv_sign2)
 ZEND_FUNCTION(ipriv_verify)
 {
 	char *content;
-	int content_len;
+	my_int content_len;
 	char *key;
-	int key_len;
+	my_int key_len;
 	IPRIV_KEY pub_key;
 	const char *dst = 0;
 	int ndst = 0;
 	int rc = DEFAULT_ERROR;
-	long serial = 0;
+	my_long serial = 0;
 
 
 	array_init(return_value);
@@ -183,7 +223,7 @@ ZEND_FUNCTION(ipriv_verify)
 
 	add_next_index_long(return_value, rc);
 	if (!rc && ndst > 0 && dst)
-		add_next_index_stringl(return_value, (char *) dst, ndst, 1);
+		my_add_next_index_stringl(return_value, (char *) dst, ndst, 1);
 	else
 		add_next_index_null(return_value);
 }
@@ -191,15 +231,15 @@ ZEND_FUNCTION(ipriv_verify)
 ZEND_FUNCTION(ipriv_verify2)
 {
 	char *content;
-	int content_len;
+	my_int content_len;
 	char *signature;
-	int signature_len;
+	my_int signature_len;
 	char *key;
-	int key_len;
+	my_int key_len;
 	IPRIV_KEY pub_key;
 	int rc = DEFAULT_ERROR;
 	int result = -1;
-	long serial = 0;
+	my_long serial = 0;
 
 
 	array_init(return_value);
@@ -222,13 +262,13 @@ ZEND_FUNCTION(ipriv_verify2)
 ZEND_FUNCTION(ipriv_encrypt)
 {
 	char *content;
-	int content_len;
+	my_int content_len;
 	char *key;
-	int key_len;
+	my_int key_len;
 	IPRIV_KEY pub_key;
 	char dst[1024];		// requires 256 bytes for 2048 bit keys
 	int rc = DEFAULT_ERROR;
-	long serial = 0;
+	my_long serial = 0;
 
 	array_init(return_value);
 	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss|l", &content, &content_len,
@@ -246,7 +286,7 @@ ZEND_FUNCTION(ipriv_encrypt)
 
 	add_next_index_long(return_value, rc);
 	if (rc && dst)
-		add_next_index_stringl(return_value, (char *) dst, rc, 1);
+		my_add_next_index_stringl(return_value, (char *) dst, rc, 1);
 	else
 		add_next_index_null(return_value);
 }
@@ -254,11 +294,11 @@ ZEND_FUNCTION(ipriv_encrypt)
 ZEND_FUNCTION(ipriv_decrypt)
 {
 	char *content;
-	int content_len;
+	my_int content_len;
 	char *key;
-	int key_len;
+	my_int key_len;
 	char *pass;
-	int pass_len;
+	my_int pass_len;
 	IPRIV_KEY sec_key;
 	char dst[1024];
 	int rc = DEFAULT_ERROR;
@@ -281,7 +321,7 @@ ZEND_FUNCTION(ipriv_decrypt)
 
 	add_next_index_long(return_value, rc);
 	if (rc > 0)
-		add_next_index_stringl(return_value, dst, rc, 1);
+		my_add_next_index_stringl(return_value, dst, rc, 1);
 	else
 		add_next_index_null(return_value);
 
